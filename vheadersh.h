86fefbcb1655bb451900a383089589ae0a6d5957
victor <victor.mecdoors@gmail.com>
2023-07-19 04:37:08 -0700

    adding functions

--- vheadersh.h
+++ vheadersh.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
-
+void hndmulti_cmd(char *input_buffer, char *multi_command[]);
+void crt_process(char *argv_buffer[]);
+void exche_cute(char *argv_buffer[], char *new_arg);
+int buiin_cmd(char **argv);
