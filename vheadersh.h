7d434d804fb70c3a5f3e9f79c26b01d6423d6844
victor <victor.mecdoors@gmail.com>
2023-07-26 04:39:55 -0700

    adding functions to header

--- vheadersh.h
+++ vheadersh.h
@@ -1,10 +1,41 @@
-#define _VICO_JJ_H
-#ifndef _VICO_JJ_H
+#ifndef VICO_H
+#define VICO_H
+
+#define MAX_INPUT_LENGTH 1024
+#define MAXARGS 128
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+#include <limits.h>
+extern char **environ;
+
 
 void hndmulti_cmd(char *input_buffer, char *multi_command[]);
 void crt_process(char *argv_buffer[]);
 void exche_cute(char *argv_buffer[], char *new_arg);
 int buiin_cmd(char **argv);
+/* code */
+void inerive_mod(int bytes, char *input_buffer,
+char *argv_buffer[], char *new_arg);
+void nonive_mod(char *input_buffer, char *multi_command[],
+char *argv_buffer[], char *new_arg);
+/* code2 */
+void crt_process(char *argv_buffer[]);
+void exche_cute(char *argv_buffer[], char *new_arg);
+int buiin_cmd(char **argv);
+/* code 3 */
+int _strlen(char *s);
+void _strncpy(char *dest, char *scsr, int n);
+int _strcmp(char *gh1, char *gh2);
+int _atoi(char *s);
+/* code4 */
+void hndmulti_cmd(char *input_buffer, char *multi_command[]);
+/* end of shell */
+#endif
